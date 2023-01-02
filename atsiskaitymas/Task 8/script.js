26afaed5152e1e20d8bd2254bd6afbef716a181d
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-02 14:28:12 +0200

    task8 done

--- atsiskaitymas/Task 8/script.js
+++ atsiskaitymas/Task 8/script.js
@@ -7,12 +7,27 @@ division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
 
 class Calculator{
-   constructor(numberOne, numberTwo){
-      this.numberOne = numberOne;
-      this.numberTwo = numberTwo;
+   constructor(){
    }
 
-   sum(){
-      return this.numberOne+this.numberTwo;
+   sum(numberOne, numberTwo){
+      return numberOne+numberTwo;
+   }
+
+   subtraction(numberOne, numberTwo){
+      return numberOne-numberTwo;
+   }
+
+   multiplication(numberOne, numberTwo){
+      return numberOne*numberTwo;
+   }
+
+   division(numberOne, numberTwo){
+      return numberOne/numberTwo;
    }
 }
+
+console.log("Sum (5+2234)", new Calculator().sum(5,2234));
+console.log("Subtraction (5-2234)", new Calculator().subtraction(5,2234));
+console.log("Multiplication (5*2234)", new Calculator().multiplication(5,2234));
+console.log("Division (5/2234)", new Calculator().division(5,2234));
