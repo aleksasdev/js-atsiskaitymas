4eac3917b9584e8ddd06791c0851ba26fbde5bef
Aleksas <aleksas.ivanauskas23@gmail.com>
2023-01-02 13:44:21 +0200

    task9 done

--- atsiskaitymas/Task 9/script.js
+++ atsiskaitymas/Task 9/script.js
@@ -6,3 +6,18 @@ title, director, budget
 Metodas: 
 wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie{
+   constructor(title, director, budget){
+      this.title = title;
+      this.director = director;
+      this.budget = budget;
+   }
+
+   wasExpensive(){
+      return this.budget > 10000000;
+   }
+}
+
+console.log("Movie with a budget of 1,000,000 is expensive:",new Movie(null, null, 100000).wasExpensive());
+console.log("Movie with a budget of 2,000,000,000 is expensive:",new Movie(null, null, 200000000).wasExpensive());
